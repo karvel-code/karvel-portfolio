185b377055be35c36ed86a9caa68b63023b069ae
karvel-code <velgithinji@gmail.com>
2022-11-02 22:41:32 +0300

    Add social icons to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => {
     return (
@@ -24,6 +25,23 @@ const Sidebar = () => {
                 </NavLink>
                 
             </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer"href="https://www.linkedin.com/in/elvisgithinji/" >
+                        <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" /> 
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer"href="https://github.com/karvel-code" >
+                        <FontAwesomeIcon icon={faGithub} color="4d4d4e" /> 
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer"href="https://www.linkedin.com/in/elvisgithinji/" >
+                        <FontAwesomeIcon icon={faYoutube} color="4d4d4e" /> 
+                    </a>
+                </li>
+            </ul>
        </div>
     )
 }
